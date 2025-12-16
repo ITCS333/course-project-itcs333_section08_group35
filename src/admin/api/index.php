66aa5824ce973f2ae3afbba9c8bbfe56b414187b
ealfayou <hemafemo@gmail.com>
2025-12-16 13:42:12 +0300

    project

--- src/admin/api/index.php
+++ src/admin/api/index.php
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 // TODO: Include the database connection class
 // Assume the Database class has a method getConnection() that returns a PDO instance
-require_once 'db.php'
+require_once 'db.php';
 
 // TODO: Get the PDO database connection
 $db = getDBConnection();
