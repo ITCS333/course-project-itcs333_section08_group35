3a76dd8ce65a61be023db012fec9be6d1dcc219a
MohamedALaswad <moahmed_1230321@yahoo.com>
2025-12-18 18:07:05 +0300

    FIXING

--- src/weekly/list.js
+++ src/weekly/list.js
@@ -51,7 +51,7 @@ function createWeekArticle(week) {
 async function loadWeeks() {
   try {
     // 1. Fetch data from 'weeks.json'
-    const response = await fetch('weeks.json');
+    const response = await fetch('../api/weeks.json')
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
